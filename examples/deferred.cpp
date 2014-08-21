9ddf269234fe8617d72554be8a69062039f2684e
danvil <davidw@danvil.de>
2014-08-21 17:26:17 +0200

    added more lights

--- examples/deferred.cpp
+++ examples/deferred.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cmath>
 
 namespace obj
 {
@@ -466,6 +467,24 @@ public:
 	std::vector<std::shared_ptr<LightObject>> lights_;
 };
 
+Eigen::Vector3f HSL(float hue, float sat, float light)
+{
+	float c = (1.0f - std::abs(2.0f*light - 1.0f)) * sat;
+	while(hue < 0) hue += 1;
+	while(hue >= 1) hue -= 1;
+	float h = hue * 6.0f;
+	float x = c * (1.0f - std::abs(std::fmod(h, 2.0f) - 1.0f));
+	float r,g,b;
+	     if(h < 1) { r = c; g = x; b = 0; }
+	else if(h < 2) { r = x; g = c; b = 0; }
+	else if(h < 3) { r = 0; g = c; b = x; }
+	else if(h < 4) { r = 0; g = x; b = c; }
+	else if(h < 5) { r = x; g = 0; b = c; }
+	else           { r = c; g = 0; b = x; }
+	float m = light - 0.5f * c;
+	return {r+m, g+m, b+m};
+}
+
 int main(void)
 {
 	std::cout << "Starting engine" << std::endl;
@@ -481,13 +500,16 @@ int main(void)
 		g_camera.view = pastry::lookAt(3*Eigen::Vector3f{2,4,3},{0,0,0},{0,0,-1});
 	}
 
+	constexpr float SPACE = 3.0f;
+
 	// geometry
 	{
 		constexpr int R = 3;
 		for(int x=-R; x<=+R; x++) {
 			for(int y=-R; y<=+R; y++) {
 				auto geom = std::make_shared<MeshObject>("assets/suzanne.obj");
-				Eigen::Affine3f pose = Eigen::Translation3f(3.0f*Eigen::Vector3f(x,y,0)) *  Eigen::AngleAxisf(0.0f,Eigen::Vector3f{0,0,1});
+				Eigen::Affine3f pose = Eigen::Translation3f(Eigen::Vector3f(SPACE*x,SPACE*y,0))
+					* Eigen::AngleAxisf(0.0f,Eigen::Vector3f{0,0,1});
 				geom->setPose(pose.matrix());
 				dr->add(geom);				
 			}
@@ -495,16 +517,28 @@ int main(void)
 	}
 
 	// lights
+	// {
+	// 	auto light = std::make_shared<LightObject>();
+	// 	light->setLightPosition({+3,3,4});
+	// 	dr->add(light);
+	// }
+	// {
+	// 	auto light = std::make_shared<LightObject>();
+	// 	light->setLightPosition({-4,1,4});
+	// 	light->setLightColor({1,0,0});
+	// 	dr->add(light);
+	// }
 	{
-		auto light = std::make_shared<LightObject>();
-		light->setLightPosition({+3,3,4});
-		dr->add(light);
-	}
-	{
-		auto light = std::make_shared<LightObject>();
-		light->setLightPosition({-4,1,4});
-		light->setLightColor({1,0,0});
-		dr->add(light);
+		constexpr int R = 3;
+		for(int x=-R; x<=+R; x++) {
+			for(int y=-R; y<=+R; y++) {
+				auto light = std::make_shared<LightObject>();
+				light->setLightPosition({SPACE*x,SPACE*y,1.5});
+				light->setLightColor(HSL(std::atan2(y,x)/6.2831853f,1.0f,0.5f));
+				light->setLightFalloff(1.65f);
+				dr->add(light);
+			}
+		}
 	}
 
 	std::cout << "Running main loop" << std::endl;
